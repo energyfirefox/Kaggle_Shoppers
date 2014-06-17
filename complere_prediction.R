f5997a51a8decac44237729f249271425d26c718
Anastasiia Kornilova <energyfirefox@gmail.com>
2014-06-17 18:27:10 +0300

    Update complere_prediction.R

--- complere_prediction.R
+++ complere_prediction.R
@@ -3,6 +3,10 @@ library(caret)
 train <- read.csv("reshaped_comp_train.csv")
 test <- read.csv("reshaped_comp_test.csv")
 
+# train <- read.csv("complete_train.csv")
+# test <- read.csv("complete_test.csv")
+
+str(train)
 
 featurePlot(train_cut[,-c(1,2)], train_cut$repeater)
 
@@ -13,9 +17,15 @@ train_cut$repeater <- as.factor(train_cut$repeater)
 
 
 ##
-model_short <- train(repeater ~., data = train[, -c(1, 4)], preProcess="pca", method ="glm")
+# model_short <- train(repeater ~., data = train[, -c(1, 3, 4, 5, 6,  8)], preProcess="pca", method ="glm")
+model_short <- train(repeater ~., data = train[, -c(1,4 )], preProcess="pca", method ="glm")
 model_full <- train(repeater ~., data = train[, -c(1, 4)],  method ="glm")
-pca <- predict(model_short,  newdata=test[,-c(1,3)])
+model_rf <- train(repeater ~., data = train[, -c(1,4 )], method ="rf", prox=TRUE)
+
+str(test)
+str(train)
+
+pca <- predict(model_short,  newdata=test[,-c(1:5, 7)])
 
 pca <- predict(model_full,  newdata=test[,-c(1,3)])
 head(pca)
